ba661fbe6a93ae61c26dc707c90d4367763f1f3e
Simone <simone.procaccia@gmail.com>
2017-12-05 17:19:39 +0100

    Simone 05/12 // implemented all timing func for bst, remove need fix. Merging URGENT AF

--- ttest.py
+++ ttest.py
@@ -1,5 +1,6 @@
 import time
 import sorting
+import bst
 import timeit
 import platform
 import random
@@ -9,7 +10,7 @@ import pandas
 import matplotlib.pyplot as plt
 
 DEFAULT_NUMBER = 100000  # 100k
-DEFAULT_POPULATION = range(1000000)  # 1m
+DEFAULT_POPULATION = [10,100,1000,10000,100000]  # 1m
 
 
 class TimeTest(object):
@@ -26,7 +27,7 @@ class TimeTest(object):
     def __init__(self, array=None, max_val=DEFAULT_NUMBER):
         # this is the dict of dicts where the values obtained from the test_it func will be stored.
         self.test_result = dict(quick_sort={}, merge_sort={}, binary_insertion={}, binary_get_random={},
-                                binary_delete={},
+                                binary_delete={}, binary_get_max={},
                                 heap_insert={}, heap_get_max={}, heap_remove={})
         # dict of all the list to use for testing
         self.array_pool = {}
@@ -54,11 +55,28 @@ class TimeTest(object):
 
             self._test_it_quick_sort(arr, key)
             self._test_it_merge_sort(arr, key)
+        print("sorting done!")
+        # BSTs implementation
+        # TODO implement the bst module
+        # the idea was to get the maximum lenght dict and populate it with timing and storing the values in
+        # self.test_result as {'bst_insertion': {bst.size(): time}}
 
-            # BSTs implementation
-            # TODO implement the bst module
-            # the idea was to get the maximum lenght dict and populate it with timing and storing the values in
-            # self.test_result as {'bst_insertion': {bst.size(): time}}
+        insertion_counter, tree = 0, bst.BinarySearchTree()
+        for i in range(DEFAULT_POPULATION[-1]):
+            insertion_counter += 1
+            if insertion_counter in DEFAULT_POPULATION:
+                self._test_it_binary_get_max(i, tree)
+                self._test_it_binary_get_random(i, tree)
+                self._test_it_binary_insertion(0, tree, i)
+            else:
+                tree.put(i, 0)
+        print("insertion timing done!")
+        # TODO this function doesn't work to me, but we'll see
+        for i in range(DEFAULT_POPULATION[-1]):
+            if i in DEFAULT_POPULATION:
+                self._test_it_binary_remove(i, tree)
+            else:
+                tree.delete(i)
 
     def _test_it_quick_sort(self, arr, key):
         self.test_result['quick_sort'][key] = timeit.timeit("sorting.quick_sort(" + str(arr) + ")",
@@ -67,6 +85,23 @@ class TimeTest(object):
     def _test_it_merge_sort(self, arr, key):
         self.test_result['merge_sort'][key] = timeit.timeit("sorting.merge(" + str(arr) + ")", globals=globals(),
                                                             number=10)
+
+    def _test_it_binary_insertion(self, val, tree, key):
+        self.test_result['binary_insertion'][key] = timeit.timeit("tree.put(" + str(key) + "," + val + ")",
+                                                                  globals=globals(), number=1)
+
+    def _test_it_binary_remove(self, key, tree):
+        self.test_result["binary_delete"][key] = timeit.timeit("tree.delete(" + str(key) + ")", globals=globals(),
+                                                               number=1)
+
+    def _test_it_binary_get_max(self, key, tree):
+        self.test_result['binary_get_max'][key] = timeit.timeit("tree.findMax()", globals=globals(), number=10)
+
+    def _test_it_binary_get_random(self, key, tree):
+        self.test_result['binary_get_random'][key] = timeit.timeit("tree.get(key)",
+                                                                   setup="key = random.randint(0, len(tree)-1)",
+                                                                   globals=globals(),
+                                                                   number=10)
 
     def csv(self, name='test' + "right now"):  # TODO implement a right now stringer
         """generates a csv file with the results of the test_it function, returns a "Run test_it before requesting
